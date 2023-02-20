6d2f6f14bc4773ca362cb110f2b3152c4bb01383
Sebastian Friedel <sebastian@civist.com>
2023-02-20 14:00:26 +0100

    minimal working example

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { Localized } from '@nubolab-ffwd/svelte-fluent';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Localized id="hello" />
